c41848f2bfeaab91f1a12960800d2d32128e6673
Juamal Blackman <juamal.blackman@digital.hmrc.gov.uk>
2023-06-15 17:04:25 +0100

    [SASS-5124] inital commit

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 
 # income-tax-tailor-return
 
-This is a placeholder README.md for a new repository
+This is where we retrieve users tailoring section data for their income tax return.
+
+## Running the service locally
+
+You will need to have the following:
+- Installed [MondoDB](https://docs.mongodb.com/manual/installation/)
+- Installed/configured [service manager](https://github.com/hmrc/service-manager).
+
+The service manager profile for this service is:
+
+    sm --start INCOME_TAX_TAILOR_RETURN
+
+This service runs on port: `localhost:9383`
+
+Run the following command to start the remaining services locally:
+
+    sudo mongod (If not already running)
+    sm --start INCOME_TAX_SUBMISSION_ALL -r
 
 ### License
 
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,20 +1,41 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import play.core.PlayVersion.current
 import sbt._
+
 
 object AppDependencies {
 
   private val bootstrapVersion = "7.16.0"
-  private val hmrcMongoVersion = "1.3.0"
+  private val hmrcMongoVersion = "1.2.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % hmrcMongoVersion
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala" % "2.14.2"
   )
 
-  val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % hmrcMongoVersion            % Test,
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % "test, it",
+    "com.typesafe.play" %% "play-test" % current % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % Test,
+    "org.scalamock" %% "scalamock" % "5.2.0" % Test,
+    "org.scalatest" %% "scalatest" % "3.2.15" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.64.0" % "test, it",
+    "com.github.tomakehurst" % "wiremock-jre8" % "2.35.0" % "test, it"
   )
 }
