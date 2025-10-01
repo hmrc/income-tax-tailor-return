8e3f543cff64ad4631eebfbd9e2ddb56b073c21d
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2025-10-01 10:15:39 +0100

    MIPR-1975 updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,14 +19,14 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val bootstrapVersion = "10.2.0"
+  private val hmrcMongoVersion = "2.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.19.2",
-    "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.3.0"
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0",
+    "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
