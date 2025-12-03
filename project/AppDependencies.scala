c7ef4778028919fe094ba87fe436f60f7a91ecd0
nadinJ <63252254+nadinJ@users.noreply.github.com>
2025-12-03 13:51:24 +0000

    MIPR-2271: Dep updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,13 +19,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.2.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val bootstrapVersion = "10.4.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.0",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.1",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.4.0"
   )
 
