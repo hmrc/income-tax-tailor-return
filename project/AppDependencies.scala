304a9e4ca39f7d1d24214ca8b061e58b2d956b37
Farnoosh Moayeri <91014204+Farnooshmo@users.noreply.github.com>
2026-02-27 12:06:15 +0000

    Upgrade dependencies (#35)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,13 +19,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.6.0"
   private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.0",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.21.1",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"        % "8.4.0"
   )
 
